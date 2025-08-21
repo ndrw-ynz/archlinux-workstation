f94314006a0bd4c6a9974c482ad4cdfeb802c76a
ndrw-ynz <andrewyanza4@gmail.com>
2025-08-21 19:57:47 +0800

    re-organize configs and plugins dir structure, with addition of new nvim ui plugins

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,30 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+local map = vim.keymap.set
+
+map('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Quit terminal mode
+map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- TIP: Disable arrow keys in normal mode
+-- map('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+-- map('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+-- map('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+-- map('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+-- Switch focus between windows with <hjkl>
+map('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+map('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+map('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+map('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
+-- map("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
+-- map("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
+-- map("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
+-- map("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,79 @@
+local opt = vim.opt
+
+-- [[ Setting options ]]
+-- See `:help o`
+-- NOTE: You can change these options as you wish!
+--  For more options, you can see `:help option-list`
+
+-- Make line numbers default
+opt.number = true
+-- You can also add relative line numbers, to help with jumping.
+--  Experiment for yourself to see if you like it!
+-- opt.relativenumber = true
+
+-- Enable mouse mode, can be useful for resizing splits for example!
+opt.mouse = 'a'
+
+-- Don't show the mode, since it's already in the status line
+opt.showmode = false
+
+-- Sync clipboard between OS and Neo
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+  opt.clipboard = 'unnamedplus'
+end)
+
+-- Enable break indent
+opt.breakindent = true
+
+-- Save undo history
+opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+opt.ignorecase = true
+opt.smartcase = true
+
+-- Keep signcolumn on by default
+opt.signcolumn = 'yes'
+
+-- Decrease update time
+opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+opt.splitright = true
+opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+--
+--  Notice listchars is set using `opt` instead of `o`.
+--  It is very similar to `o` but offers an interface for conveniently interacting with tables.
+--   See `:help lua-options`
+--   and `:help lua-options-guide`
+opt.list = true
+opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+opt.inccommand = 'split'
+
+-- Show which line your cursor is on
+opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+opt.scrolloff = 10
+
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+opt.confirm = true
+
+-- Consistent identation
+opt.tabstop = 2 -- render TABs as 2 spaces
+opt.shiftwidth = 2 -- indent by 2 spaces
+opt.expandtab = true -- convert TABs to spaces
