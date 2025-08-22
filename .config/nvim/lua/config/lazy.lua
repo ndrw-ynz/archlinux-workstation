2ceaf6f73b90f86c7d1e1c15db22d20292057839
ndrw-ynz <andrewyanza4@gmail.com>
2025-08-22 18:10:31 +0800

    add C++ LSP and language support and enable mini.files plugin support

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -19,6 +19,7 @@ require('lazy').setup {
     { import = 'plugins.lsp' },
     { import = 'plugins.tools' },
     { import = 'plugins.ui' },
+    { import = 'plugins.utils' },
     'NMAC427/guess-indent.nvim',
     { -- Useful plugin to show you pending keybinds.
       'folke/which-key.nvim',
@@ -268,7 +269,7 @@ require('lazy').setup {
 
             -- Jump to the definition of the word under your cursor.
             --  This is where a variable was first declared, or where a function is defined, etc.
-            --  To jump back, press <C-t>.
+
             map('grd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
 
             -- WARN: This is not Goto Definition, this is Goto Declaration.
@@ -387,7 +388,7 @@ require('lazy').setup {
         --  - settings (table): Override the default settings passed when initializing the server.
         --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
         local servers = {
-          -- clangd = {},
+          clangd = {},
           -- gopls = {},
           -- pyright = {},
           -- rust_analyzer = {},
@@ -484,6 +485,7 @@ require('lazy').setup {
         end,
         formatters_by_ft = {
           lua = { 'stylua' },
+          cpp = { 'clang-format' },
           -- Conform can also run multiple formatters sequentially
           -- python = { "isort", "black" },
           --
@@ -592,28 +594,6 @@ require('lazy').setup {
       },
     },
 
-    { -- You can easily change to a different colorscheme.
-      -- Change the name of the colorscheme plugin below, and then
-      -- change the command in the config to whatever the name of that colorscheme is.
-      --
-      -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-      'folke/tokyonight.nvim',
-      priority = 1000, -- Make sure to load this before all the other start plugins.
-      config = function()
-        ---@diagnostic disable-next-line: missing-fields
-        require('tokyonight').setup {
-          styles = {
-            comments = { italic = false }, -- Disable italics in comments
-          },
-        }
-
-        -- Load the colorscheme here.
-        -- Like many other themes, this one has different styles, and you could load
-        -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-        vim.cmd.colorscheme 'tokyonight-night'
-      end,
-    },
-
     -- Highlight todo, notes, etc in comments
     { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
@@ -634,6 +614,9 @@ require('lazy').setup {
         -- - sd'   - [S]urround [D]elete [']quotes
         -- - sr)'  - [S]urround [R]eplace [)] [']
         require('mini.surround').setup()
+
+        -- Mini-files to navigate and manipulate file system
+        require('mini.files').setup()
 
         -- Simple and easy statusline.
         --  You could remove this setup call if you don't like it,
@@ -660,7 +643,7 @@ require('lazy').setup {
       main = 'nvim-treesitter.configs', -- Sets main module to use for opts
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
       opts = {
-        ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+        ensure_installed = { 'bash', 'c', 'cpp', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
         -- Autoinstall languages that are not installed
         auto_install = true,
         highlight = {
