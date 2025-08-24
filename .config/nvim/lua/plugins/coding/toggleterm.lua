45c9a16d2341c26a8741942a1de04b6637867e0e
ndrw-ynz <andrewyanza4@gmail.com>
2025-08-24 22:26:16 +0800

    add toggleterm.nvim config

---
+++ .config/nvim/lua/plugins/coding/toggleterm.lua
@@ -0,0 +1,14 @@
+return {
+  'akinsho/toggleterm.nvim',
+  version = '*',
+  opts = {
+    config = true,
+    open_mapping = [[<c-\>]],
+    shade_terminals = true,
+    direction = 'horizontal',
+    auto_scroll = true,
+    float_opts = {
+      border = 'curved',
+    },
+  },
+}
