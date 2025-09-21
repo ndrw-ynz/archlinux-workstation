4e444f29a50641c13b90a278e87b323efc425073
ndrw-ynz <andrewyanza4@gmail.com>
2025-09-21 12:30:28 +0800

    delegate lazygit open keymap to toggleterm

--- .config/nvim/lua/plugins/coding/toggleterm.lua
+++ .config/nvim/lua/plugins/coding/toggleterm.lua
@@ -2,7 +2,6 @@ return {
   'akinsho/toggleterm.nvim',
   version = '*',
   opts = {
-    config = true,
     open_mapping = [[<c-\>]],
     shade_terminals = true,
     direction = 'horizontal',
@@ -10,5 +9,26 @@ return {
     float_opts = {
       border = 'curved',
     },
+    persist_mode = true,
+    start_in_insert = true,
+    close_on_exit = true,
+    shell = vim.o.shell,
   },
+  config = function(_, opts)
+    require('toggleterm').setup(opts)
+
+    -- Define LazyGit terminal
+    local Terminal = require('toggleterm.terminal').Terminal
+    local lazygit = Terminal:new { cmd = 'lazygit', hidden = true, direction = 'float' }
+
+    function _lazygit_toggle()
+      lazygit:toggle()
+    end
+
+    -- Keymap registered AFTER function exists
+    vim.keymap.set('n', '<leader>gg', _lazygit_toggle, {
+      desc = 'Run LazyGit',
+      silent = true,
+    })
+  end,
 }
